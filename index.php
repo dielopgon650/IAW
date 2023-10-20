16f08171a0e7e445701d59c637ca68c23b607375
dielopgon650 <dielopgon@alu.edu.gva.es>
2023-10-20 14:12:52 +0200

    ultimo

--- index.php
+++ index.php
@@ -3,9 +3,6 @@
     <head> Hola</head>
 <body>
     <?php 
-    ini_set('display_errors',1);
-    ini_set('display_startup_errors',1);
-    error_reporting(E_ALL);
     echo "Mi primer programa PHP";
     ?>
 </body>
